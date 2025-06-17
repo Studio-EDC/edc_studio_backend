0a78fc1a798c52ccfa43fddd6c39f77b12c2de50
Itziar Mensa Minguito <itziar@Itziars-MacBook-Air.local>
2025-06-17 08:53:16 +0200

    crud contracts finished

--- app/routes/contracts_routes.py
+++ app/routes/contracts_routes.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.models.contract import Contract
-from app.services.contracts_service import create_contract, get_contracts_by_edc_id
+from app.services.contracts_service import create_contract, delete_contract, get_contract_by_contract_id_service, get_contracts_by_edc_id, update_contract
 
 router = APIRouter()
 
@@ -14,3 +14,24 @@ async def create_contract_route(data: Contract):
 @router.get("/by-edc/{edc_id}", response_model=list[Contract])
 async def list_contracts_by_edc(edc_id: str):
     return await get_contracts_by_edc_id(edc_id)
+
+
+@router.get("/by-contract-id/{edc_id}/{contract_id}", response_model=Contract)
+async def get_contract_by_contract_id(edc_id: str, contract_id: str):
+    return await get_contract_by_contract_id_service(edc_id, contract_id)
+
+
+@router.put("/{edc_id}")
+async def update_contract_route(contract: Contract, edc_id: str):
+    success = await update_contract(contract, edc_id)
+    if not success:
+        raise HTTPException(status_code=404, detail="Asset not found or not updated")
+    return {"message": "Asset updated successfully"}
+
+
+@router.delete("/{contract_id}/{edc_id}")
+async def delete_contract_route(contract_id: str, edc_id: str):
+    success = await delete_contract(contract_id, edc_id)
+    if not success:
+        raise HTTPException(status_code=404, detail="Asset not found or not deleted")
+    return {"message": "Asset deleted successfully"}
--- app/services/contracts_service.py
+++ app/services/contracts_service.py
@@ -15,8 +15,6 @@ async def create_contract(data: Contract) -> str:
 
     contract_dict = data.model_dump()
 
-    print(contract_dict)
-
     try:
         return await register_contract_with_edc(contract_dict, connector)
     except Exception as e:
@@ -135,6 +133,169 @@ async def get_contracts_by_edc_id(edc_id: str) -> list[dict]:
 
         return contracts
 
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(status_code=e.response.status_code, detail=f"HTTP error from EDC: {e.response.text}")
+    except httpx.RequestError as e:
+        raise HTTPException(status_code=502, detail=f"Connection error to EDC: {str(e)}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
+    
+
+async def get_contract_by_contract_id_service(edc_id: str, asset_id: str) -> Contract:
+    db = get_db()
+
+    try:
+        connector = await db["connectors"].find_one({"_id": ObjectId(edc_id)})
+        if not connector:
+            raise HTTPException(status_code=404, detail="EDC not found")
+        
+        if connector["mode"] == "managed":
+            management_port = connector["ports"]["management"]
+            base_url = f"http://localhost:{management_port}/management/v3/contractdefinitions/{asset_id}"
+        elif connector["mode"] == "remote":
+            base_url = f"{connector['endpoints_url']['management'].rstrip('/')}/management/v3/contractdefinitions/{asset_id}"
+        else:
+            raise ValueError("Invalid connector mode")
+
+        api_key = connector["api_key"]
+        if not api_key:
+            raise HTTPException(status_code=500, detail="Connector API key not configured")
+
+        headers = {
+            "x-api-key": api_key
+        }
+
+        async with httpx.AsyncClient() as client:
+            response = await client.get(base_url, headers=headers)
+            response.raise_for_status()
+            item = response.json()
+
+        assets = []
+
+        assets_selector = item.get("assetsSelector")
+
+        if isinstance(assets_selector, dict):
+            # Un solo Criterion
+            assets.append(assets_selector.get("operandRight"))
+        elif isinstance(assets_selector, list):
+            # Lista de Criterion
+            for criterion in assets_selector:
+                assets.append(criterion.get("operandRight"))
+
+        contract = Contract(
+            edc=edc_id,
+            contract_id=item.get("@id"),
+            accessPolicyId=item.get("accessPolicyId"),
+            contractPolicyId=item.get("contractPolicyId"),
+            assetsSelector=assets,
+            context=item.get("@context", {"@vocab": "https://w3id.org/edc/v0.0.1/ns/"})
+        )
+        
+        return contract
+
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(status_code=e.response.status_code, detail=f"HTTP error from EDC: {e.response.text}")
+    except httpx.RequestError as e:
+        raise HTTPException(status_code=502, detail=f"Connection error to EDC: {str(e)}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
+    
+
+async def update_contract(contract: Contract, edc_id: str) -> bool:
+    db = get_db()
+
+    try:
+        connector = await db["connectors"].find_one({"_id": ObjectId(edc_id)})
+        if connector["mode"] == "managed":
+            management_port = connector["ports"]["management"]
+            base_url = f"http://localhost:{management_port}/management/v3/contractdefinitions"
+        elif connector["mode"] == "remote":
+            base_url = f"{connector['endpoints_url']['management'].rstrip('/')}/management/v3/contractdefinitions"
+        else:
+            raise ValueError("Invalid connector mode")
+        
+        api_key = connector["api_key"]
+        if not api_key:
+            raise HTTPException(status_code=500, detail="Connector API key not configured")
+        
+        payload = {
+            "@id": contract.contract_id,
+            "@type": "ContractDefinition",
+            "accessPolicyId": contract.accessPolicyId,
+            "contractPolicyId": contract.contractPolicyId,
+            "@context": {
+                "@vocab": "https://w3id.org/edc/v0.0.1/ns/",
+                "edc": "https://w3id.org/edc/v0.0.1/ns/",
+                "odrl": "http://www.w3.org/ns/odrl/2/"
+            }
+        }
+
+        if len(contract.assetsSelector) == 1:
+            payload["assetsSelector"] = {
+                "@type": "Criterion",
+                "operandLeft": "id",
+                "operator": "=",
+                "operandRight": contract.assetsSelector[0]
+            }
+        elif len(contract.assetsSelector) > 1:
+            payload["assetsSelector"] = [
+                {
+                    "@type": "Criterion",
+                    "operandLeft": "id",
+                    "operator": "=",
+                    "operandRight": asset
+                }
+                for asset in contract.assetsSelector
+            ]
+
+
+        headers = {
+            "x-api-key": api_key
+        }
+
+        async with httpx.AsyncClient() as client:
+            response = await client.put(base_url, json=payload, headers=headers)
+            if response.status_code == 204:
+                return True
+            else:
+                return False
+    
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(status_code=e.response.status_code, detail=f"HTTP error from EDC: {e.response.text}")
+    except httpx.RequestError as e:
+        raise HTTPException(status_code=502, detail=f"Connection error to EDC: {str(e)}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
+    
+
+async def delete_contract(contract_id: str, edc_id: str) -> bool:
+    db = get_db()
+
+    try:
+        connector = await db["connectors"].find_one({"_id": ObjectId(edc_id)})
+        if connector["mode"] == "managed":
+            management_port = connector["ports"]["management"]
+            base_url = f"http://localhost:{management_port}/management/v3/contractdefinitions/{contract_id}"
+        elif connector["mode"] == "remote":
+            base_url = f"{connector['endpoints_url']['management'].rstrip('/')}/management/v3/contractdefinitions/{contract_id}"
+        else:
+            raise ValueError("Invalid connector mode")
+        
+        api_key = connector["api_key"]
+        if not api_key:
+            raise HTTPException(status_code=500, detail="Connector API key not configured")
+
+        headers = {
+            "x-api-key": api_key
+        }
+
+        async with httpx.AsyncClient() as client:
+            response = await client.delete(base_url, headers=headers)
+            if response.status_code == 204:
+                return True
+            else:
+                return False
+    
     except httpx.HTTPStatusError as e:
         raise HTTPException(status_code=e.response.status_code, detail=f"HTTP error from EDC: {e.response.text}")
     except httpx.RequestError as e:
